15caa6f698dbb4bb8c9849da6886c46034e4ef07
Kyle Lirette <162719550+kylelirette93@users.noreply.github.com>
2025-10-07 16:25:19 -0300

    Implemented door system.

--- Assets/Scripts/Systems/DoorSystem/Door.cs
+++ Assets/Scripts/Systems/DoorSystem/Door.cs
@@ -2,9 +2,16 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-
+    public KeyDataSO keyToUnlock;
     private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.gameObject.CompareTag("Player"))
+        {
+            PlayerInventory inventory = other.gameObject.GetComponent<PlayerInventory>();
+            if (inventory.Keys.Contains(keyToUnlock))
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 }
--- Assets/Scripts/Systems/DoorSystem/KeyDataSO.cs
+++ Assets/Scripts/Systems/DoorSystem/KeyDataSO.cs
@@ -3,5 +3,5 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Data", menuName = "ScritableObjects/Keys", order = 1)]
 public class KeyDataSO : ItemDataSO
 {
-    public int doorID;
+
 }
