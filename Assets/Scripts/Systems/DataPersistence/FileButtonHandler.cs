db370f90c62107b1844ec1b32f563bf29e521d92
Kyle Lirette <162719550+kylelirette93@users.noreply.github.com>
2025-10-23 13:26:44 -0300

    Added modification for button.
    Changes size of button based on new game, continue or ng+.

--- Assets/Scripts/Systems/DataPersistence/FileButtonHandler.cs
+++ Assets/Scripts/Systems/DataPersistence/FileButtonHandler.cs
@@ -1,9 +1,12 @@
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 [DefaultExecutionOrder(50)]
 public class FileButtonHandler : MonoBehaviour
 {
     public DataPersistenceManager dataPersistenceManager;
+    public RectTransform button;
     public TextMeshProUGUI buttonText;
 
     private void OnEnable()
@@ -14,18 +17,27 @@ public class FileButtonHandler : MonoBehaviour
             GameData data = dataPersistenceManager.GetGameData();
             if (data.timesWon > 0)
             {
-                buttonText.text = "New Game+";
+                string ngplus = "";
+                for (int i = 0; i < data.timesWon; i++) 
+                {
+                    ngplus += "+";
+                }
+
+                button.sizeDelta = new Vector2(200, 30);
+                buttonText.text = "Continue(NG" + ngplus + ")";
+
                 return;
             }
             else
             {
+                button.sizeDelta = new Vector2(140, 30);
                 buttonText.text = "Continue";
             }
         }
         else
         {
+            button.sizeDelta = new Vector2(140, 30);
             buttonText.text = "New Game";
-        }
-        
+        }        
     }
 }
